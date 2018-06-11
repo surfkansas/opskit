cdf8e96cd671dd166abdbbd7dc6f714b242d3aec
Mark Sweat <mark.sweat@kochind.com>
2018-06-11 12:11:58 -0500

    0.8.108.3

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
 setup(
   name = 'opskit',
-  py_modules=['opskit'],
+  py_modules = ['opskit'],
   packages = ['opskit'], 
-  package_data={'': '*.txt'},
   entry_points = {
-    'console_scripts': ['opskit=opstkit:main'],
+    'console_scripts': ['opskit=opskit:main'],
   },
-  version = '0.8.108',
+  version = '0.8.108.3',
   author = 'Mark Sweat',
-  author_email = 'mark@surfkansas.com'
+  author_email = 'mark@surfkansas.com',
+  url = 'https://github.com/surfkansas/opskit', 
+  download_url = 'https://github.com/surfkansas/opskit/archive/0.8.108.3.tar.gz'
 )
